3a733dcd8643bf52210f5f27d882e87298c29cb6
Joshua Jeremia <joshua.jeremia@hotmail.com>
2023-02-18 09:23:45 +0700

    Implement Firebase Repository.

--- app/src/main/java/com/myshopproject/utils/Constants.kt
+++ app/src/main/java/com/myshopproject/utils/Constants.kt
@@ -3,8 +3,6 @@ package com.myshopproject.utils
 object Constants {
     const val BASE_URL = "https://portlan.id/training_android/public/api/ecommerce/"
 
-    const val FB_CONFIG_KEY = "payment_json"
-
     const val PICTURE_INTENT = "picture"
     const val IS_BACK_CAMERA_INTENT = "back_camera"
 
